a4230f1b1f4330958d9accb767119a69a76d498e
Vincent Davis <vincentmdavis@protonmail.com>
2020-10-21 16:21:01 -0400

    clever girl

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import gym
 from collections import defaultdict
 from random import uniform, choice
 import numpy as np
+from math import exp
 
 
 class DynaQ:
@@ -9,20 +10,20 @@ class DynaQ:
     def __init__(self, env):
         self.env = env
 
-        self.episodes = 1000
+        self.episodes = 100000
+        self.episode = 0
 
-        self.learning_episodes = 10
+        self.learning_episodes = 5
         self.planning_steps = 100
 
         self.test_cycles = 10
 
         self.alpha = 0.9
         self.gamma = 0.8
-        self.epsilon = 0.2
         self.action_space = list(range(self.env.action_space.n))
         self.state_space = np.array(range(16))
 
-        self.q_dict = np.ones((16, 4))
+        self.q_dict = np.zeros((16, 4))
         self.t_dict = np.zeros((16, 4, 16))
         self.n_dict = np.zeros((16, 4, 16))
         self.r_dict = np.zeros((16, 4, 16))
@@ -39,12 +40,15 @@ class DynaQ:
         """
         cum_reward = 0
         for i in range(self.episodes):
+            self.episode = i
             self.do_learning()
             _sum = 0
             for j in range(self.test_cycles):
                 _sum += self.do_test()
             cum_reward += _sum
-            # print(_sum)
+            if i % 1000 == 0:
+                print("Win Rate after {} episodes: {}%".format(self.episode ,round(cum_reward / (i * self.test_cycles + 1),4)*100))
+                print("{}".format(_sum/self.test_cycles))
         print(cum_reward / (self.test_cycles * self.episodes))
         print(self.q_dict)
 
@@ -64,7 +68,9 @@ class DynaQ:
                 # Q-learning
                 action = self.epsilon_greedy(current_state)
                 next_state, reward, done, _ = self.env.step(action)
-                self.q_update(current_state, next_state, action, reward)
+
+                terminal = True if next_state == 15 else False
+                self.q_update(current_state, next_state, action, reward, terminal)
 
                 # Update n,r,t dictionaries
                 self.n_dict[current_state][action][next_state] += 1
@@ -73,10 +79,12 @@ class DynaQ:
                 self.t_dict[current_state][action] = np.divide(self.n_dict[current_state][action], total_transitions)
 
                 # Q-Planning
-                self.do_planning()
+                if i > 1000:
+                    self.do_planning()
 
                 # add observed state and action, update current state
-                self.observed_states[current_state].add(action)
+                if len(self.observed_states[current_state]) != 4:
+                    self.observed_states[current_state].add(action)
                 current_state = next_state
 
         self.env.close()
@@ -91,9 +99,10 @@ class DynaQ:
             for j in range(self.planning_steps):
                 s = choice(list(self.observed_states.keys()))
                 a = choice(tuple(self.observed_states[s]))
-                s_prime = np.random.choice(self.state_space, 1, p=self.t_dict[s][a])
+                s_prime = np.random.choice(self.state_space, 1, p=self.t_dict[s][a])  # <-- super slow
                 r = self.r_dict[s][a][s_prime]
-                self.q_update(s, s_prime, a, r)
+                done = True if s_prime == 15 else False
+                self.q_update(s, s_prime, a, r, done)
 
     def do_test(self):
         """
@@ -113,7 +122,7 @@ class DynaQ:
 
         return reward
 
-    def q_update(self, s, s_prime, action, reward):
+    def q_update(self, s, s_prime, action, reward, done):
         """
         Update q dictionary.
         :param s: past state
@@ -122,9 +131,12 @@ class DynaQ:
         :param reward: reward at resulting state
         :return: None
         """
-        old_val = self.q_dict[s][action]
-        best_future_val = np.max(self.q_dict[s_prime])
-        self.q_dict[s][action] = old_val + self.alpha * (reward + self.gamma * best_future_val - old_val)
+        if done:
+            self.q_dict[s][action] += self.alpha * (reward - self.q_dict[s][action])
+        else:
+            old_val = self.q_dict[s][action]
+            best_future_val = np.max(self.q_dict[s_prime])
+            self.q_dict[s][action] = old_val + self.alpha * (reward + self.gamma * best_future_val - old_val)
 
     def epsilon_greedy(self, state):
         """
@@ -134,10 +146,19 @@ class DynaQ:
         :return: action (int)
         """
         chance = uniform(0, 1)
+        self.epsilon = self.decay_function()
         if chance < self.epsilon:
             return self.env.action_space.sample()
         else:
-            return np.argmax(self.q_dict[state])
+            _max = np.max(self.q_dict[state])
+            max_indices = np.argwhere(self.q_dict[state] == _max)
+            if len(max_indices) > 1:
+                return np.random.choice(np.concatenate(max_indices))
+            else:
+                return max_indices.item(0)
+
+    def decay_function(self):
+        return exp((-self.episode - 20000) / 20000) + 0.02
 
 
 def main():
